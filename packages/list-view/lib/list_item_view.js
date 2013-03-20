c924a31aaf52b921eb543af8b65d06f7d0e57e39
Stefan Penner <stefan.penner@gmail.com>
2013-03-20 11:33:37 -0400

    work with both css.top and 3dtransform

--- packages/list-view/lib/list_item_view.js
+++ packages/list-view/lib/list_item_view.js
@@ -1,17 +1,33 @@
 var get = Ember.get, set = Ember.set;
 
+// extract into positiongDelegate
+var applyTransform = (function(){
+  var element = document.createElement('div');
+
+  if ('webkitTransform' in element.style){
+    return function(element, x, y){
+      element.style.webkitTransform = 'translate3d(' + x + 'px, ' + y + 'px, 0)';
+    };
+  }else{
+    return function(element, x, y){
+      element.style.top =  y + 'px';
+      element.style.left = x + 'px';
+    };
+  }
+})();
+
 Ember.ListItemView = Ember.View.extend({
   classNames: ['ember-list-item-view'],
 
   _updateStyle: function() {
-    var e = get(this, 'element');
-    if (e) { // FIXME
-      var top = get(this, 'top');
-      if (this._lastTop !== top) {
-        e.style.top = top + 'px';
-        // e.style.webkitTransform = 'translate3d(0, ' + top + 'px, 0)'
-        this._lastTop = top;
-      }
+    var element = get(this, 'element');
+    if (!element) { return; }
+
+    var top = get(this, 'top');
+
+    if (this._lastTop !== top) {
+      applyTransform(element, 0, top);
+      this._lastTop = top;
     }
   },
 
--- packages/list-view/tests/list_view_test.js
+++ packages/list-view/tests/list_view_test.js
@@ -38,10 +38,31 @@ function generateContent(n) {
   return content;
 }
 
+function extractYFromTransform(string){
+  var number = string.replace(/translate3d\(0px, (\d+)px,.*$/, '$1');
+
+  return parseInt(number, 10);
+}
+
+function extractNumberFromPosition(string){
+  var number = string.replace(/px/g,'');
+  return parseInt(number, 10);
+}
+
 function itemPositions() {
   return view.$('.ember-list-item-view').toArray().map(function(e) {
-    return e.style.top;
-  }).join(' ').replace(/px/g, "");
+
+    var topOffset = null;
+
+    if(e.style.top){
+      topOffset = extractNumberFromPosition(e.style.top);
+    } else if(e.style.webkitTransform) {
+      topOffset = extractYFromTransform(e.style.webkitTransform);
+    }
+
+    return topOffset;
+
+  });
 }
 
 test("should exist", function() {
@@ -78,7 +99,7 @@ test("should render a subset of the full content, based on the height, in the co
   equal(view.$('.ember-list-item-view:eq(0)').text(), "Item 1");
   equal(view.$('.ember-list-item-view:eq(10)').text(), "Item 11");
 
-  equal(itemPositions(), "0 50 100 150 200 250 300 350 400 450 500");
+  deepEqual(itemPositions(), [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500]);
 });
 
 test("should render correctly with an initial scrollTop", function() {
@@ -101,7 +122,7 @@ test("should render correctly with an initial scrollTop", function() {
   equal(view.$('.ember-list-item-view:eq(0)').text(), "Item 10");
   equal(view.$('.ember-list-item-view:eq(10)').text(), "Item 20");
 
-  equal(itemPositions(), "450 500 550 600 650 700 750 800 850 900 950", "The rows are in the correct positions");
+  deepEqual(itemPositions(), [450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950], "The rows are in the correct positions");
 });
 
 test("should be programatically scrollable", function() {
@@ -124,7 +145,7 @@ test("should be programatically scrollable", function() {
   });
 
   equal(view.$('.ember-list-item-view').length, 11, "The correct number of rows were rendered");
-  equal(itemPositions(), "550 600 650 700 750 800 850 900 950 450 500", "The rows are in the correct positions");
+  deepEqual(itemPositions(), [550, 600, 650, 700, 750, 800, 850, 900, 950, 450, 500], "The rows are in the correct positions");
 });
 
 test("replacing the list content", function() {
@@ -163,6 +184,7 @@ test("modifying the list content", function() {
     rowHeight: rowHeight,
     itemViewClass: itemViewClass
   });
+
   appendView();
 
   Ember.run(function() {
@@ -173,52 +195,6 @@ test("modifying the list content", function() {
   equal(view.$(':last')[0].style.height, "5050px", "The scrollable view has the correct height");
 });
 
-test("item view state is maintained across scroll / content array manipulation", function() {
-  var content = generateContent(100),
-      height = 500,
-      rowHeight = 50,
-      itemViewClass = Ember.ListItemView.extend({
-        template: Ember.Handlebars.compile("{{name}}"),
-        classNameBindings: ['isSelected'],
-        prepareForReuse: function() {
-          this.set('isSelected', false);
-        },
-        serialize: function() {
-          return this.getProperties('isSelected');
-        }
-      });
-  view = Ember.ListView.create({
-    content: content,
-    height: height,
-    rowHeight: rowHeight,
-    itemViewClass: itemViewClass
-  });
-  appendView();
-
-  Ember.run(function() {
-    view.set('childViews.0.isSelected', true);
-  });
-
-  Ember.run(function() {
-    view.scrollTo(600);
-  });
-
-  equal(view.get('childViews.0.isSelected'), false, "View state was cleared after scroll");
-
-  Ember.run(function() {
-    view.scrollTo(0);
-  });
-
-  equal(view.get('childViews.0.isSelected'), true, "View state was restored after scroll");
-
-  Ember.run(function() {
-    content.popObject();
-  });
-
-  equal(view.get('childViews.0.isSelected'), true, "View state was retained after content array manipulation");
-});
-
 // TODO:
 // - selection?
-// - view state serialization
 // - content array length changes, causes the scrollTop to be greater than the totalHeight
