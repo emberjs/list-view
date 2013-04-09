1d21a58db31ab6df8966e6e7b898bedbf6462248
Stefan Penner <stefan.penner@gmail.com>
2013-04-08 20:26:19 -0400

    Some ListItemView performance

--- packages/list-view/lib/list_item_view.js
+++ packages/list-view/lib/list_item_view.js
@@ -53,66 +53,70 @@ function didInsertElementIfNeeded(view) {
   }
 }
 
-Ember.ListItemView = Ember.View.extend({
-  classNames: ['ember-list-item-view'],
-  _position: null,
+function rerender() {
+  var element, buffer, context, hasChildViews;
 
-  applyTransform: applyTransform,
+  element = get(this, 'element');
 
-  _updateStyle: function() {
-    var element, position, _position;
+  if (!element) { return; }
 
-    element = get(this, 'element');
-    position = get(this, 'position');
-    _position = this._position;
+  context = get(this, 'context');
+  hasChildViews = this._childViews.length > 0;
 
-    if (!element) { return; }
-    if (samePosition(position, _position)) { return; }
-
-    this.applyTransform(element, position);
-
-    this._position = position;
-  },
-
-  positionDidChange: Ember.observer(function(){
-    this._updateStyle();
-  }, 'position'),
-
-  didInsertElement: function() {
-    this._updateStyle();
-  },
-
-  // this will eventually go away once rerender becomes
-  // less wasteful. (and buggy)
-  _contextDidChange: Ember.observer(function() {
-    var element, buffer, context;
-
-    element = get(this, 'element');
-    if (!element) { return; }
-    context = get(this, 'context');
-
+  if (hasChildViews) {
     this.triggerRecursively('willClearRender');
     this.clearRenderedChildren();
+  }
 
-    if (context) {
-      buffer = Ember.RenderBuffer();
-      buffer = this.renderToBuffer(buffer);
+  if (context) {
+    buffer = Ember.RenderBuffer();
+    buffer = this.renderToBuffer(buffer);
 
-      this.invokeRecursively(willInsertElementIfNeeded);
-
-      element.innerHTML = buffer.innerString ? buffer.innerString() : backportedInnerString(buffer);
-
-      set(this, 'element', element);
-
-      this.transitionTo('inDOM', get(this, 'children'));
-
-      this.invokeRecursively(didInsertElementIfNeeded);
-
-      this._updateStyle();
-    } else {
-      element.innerHTML = ''; // when there is no context, this view should be completely empty
+    if (hasChildViews) {
+      this.invokeRecursively(willInsertElementIfNeeded, false);
     }
-  }, 'context'),
+
+    element.innerHTML = buffer.innerString ? buffer.innerString() : backportedInnerString(buffer);
+
+    set(this, 'element', element);
+
+    this.transitionTo('inDOM');
+
+    if (hasChildViews) {
+      this.invokeRecursively(didInsertElementIfNeeded, false);
+    }
+
+    this._updateStyle();
+  } else {
+    element.innerHTML = ''; // when there is no context, this view should be completely empty
+  }
+}
+
+function updateStyle() {
+  var element, position, _position;
+
+  element = get(this, 'element');
+  position = get(this, 'position');
+  _position = this._position;
+
+  if (!element) { return; }
+  if (samePosition(position, _position)) { return; }
+
+  this.applyTransform(element, position);
+
+  this._position = position;
+}
+
+Ember.ListItemView = Ember.View.extend({
+  classNames: ['ember-list-item-view'],
+
+  _position: null,
+  _updateStyle: updateStyle,
+  _contextDidChange: Ember.observer(rerender, 'context'),
+
+  applyTransform: applyTransform,
+  positionDidChange: Ember.observer(updateStyle, 'position'),
+  didInsertElement: updateStyle,
 
   prepareForReuse: function() {
     this._position = null;
