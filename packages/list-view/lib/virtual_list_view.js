6876ab8c6ca3fbc81e32224c88641890e00b5218
Stefan Penner <stefan.penner@gmail.com>
2013-04-10 18:15:25 -0400

    prefix custom events as not to conflict with real events

--- packages/list-view/lib/virtual_list_view.js
+++ packages/list-view/lib/virtual_list_view.js
@@ -18,13 +18,17 @@ Ember.VirtualListView = Ember.ContainerView.extend(Ember.ListViewMixin, {
     position: 'relative',
     overflow: 'hidden'
   },
+
   init: function(){
     this._super();
     this.setupScroller();
   },
+
   applyTransform: Ember.ListViewHelper.applyTransform,
+
   setupScroller: function(){
     var _this = this;
+
     this.scroller = new Scroller(function(left, top, zoom) {
       if (_this.listContainerElement) {
         _this.applyTransform(_this.listContainerElement, {x: 0, y: -top});
@@ -36,91 +40,105 @@ Ember.VirtualListView = Ember.ContainerView.extend(Ember.ListViewMixin, {
     updateScrollerDimensions.call(this);
     this.scroller.setDimensions(this.get('width'), this.get('height'), this.get('width'), this.get('totalHeight'));
   },
+
   scrollerDimensionsNeedToChange: Ember.observer(function() {
     Ember.run.once(this, updateScrollerDimensions);
   }, 'width', 'height', 'elementWidth'),
+
   didInsertElement: function() {
     var self, listContainerElement, el;
 
     self = this;
     this.listContainerElement = el = this.$('> .ember-list-container')[0];
 
-    self._touchStart = function(e) { self.touchStart(e); };
-    self._touchMove =  function(e) { self.touchMove(e);  };
-    self._touchEnd =   function(e) { self.touchEnd(e);   };
-    self._mouseDown =  function(e) { self.mouseDown(e);  };
-    self._mouseMove =  function(e) { self.mouseMove(e);  };
-    self._mouseUp =    function(e) { self.mouseUp(e);    };
-    self._mouseWheel = function(e) { self.mouseWheel(e); };
+    self.__touchStart = function(e) { self._touchStart(e); };
+    self.__touchMove =  function(e) { self._touchMove(e);  };
+    self.__touchEnd =   function(e) { self._touchEnd(e);   };
+    self.__mouseDown =  function(e) { self._mouseDown(e);  };
+    self.__mouseMove =  function(e) { self._mouseMove(e);  };
+    self.__mouseUp =    function(e) { self._mouseUp(e);    };
+    self.__mouseWheel = function(e) { self._mouseWheel(e); };
 
-    el.addEventListener('touchstart', this._touchStart);
-    el.addEventListener('touchmove',  this._touchMove);
-    el.addEventListener('touchend',   this._touchEnd);
-    el.addEventListener('mousedown',  this._mouseDown);
-    el.addEventListener('mousemove',  this._mouseMove);
-    el.addEventListener('mouseup',    this._mouseUp);
-    el.addEventListener('mousewheel', this._mouseWheel);
+    el.addEventListener('touchstart', this.__touchStart);
+    el.addEventListener('touchmove',  this.__touchMove);
+    el.addEventListener('touchend',   this.__touchEnd);
+    el.addEventListener('mousedown',  this.__mouseDown);
+    el.addEventListener('mousemove',  this.__mouseMove);
+    el.addEventListener('mouseup',    this.__mouseUp);
+    el.addEventListener('mousewheel', this.__mouseWheel);
   },
 
   willDestroyElement: function() {
     var el = this.listContainerElement;
-    el.removeEventListener('touchstart', this._touchStart);
-    el.removeEventListener('touchmove',  this._touchMove);
-    el.removeEventListener('touchend',   this._touchEnd);
-    el.removeEventListener('mousedown',  this._mouseDown);
-    el.removeEventListener('mousemove',  this._mouseMove);
-    el.removeEventListener('mouseup',    this._mouseUp);
-    el.removeEventListener('mousewheel', this._mouseWheel);
+
+    el.removeEventListener('touchstart', this.__touchStart);
+    el.removeEventListener('touchmove',  this.__touchMove);
+    el.removeEventListener('touchend',   this.__touchEnd);
+    el.removeEventListener('mousedown',  this.__mouseDown);
+    el.removeEventListener('mousemove',  this.__mouseMove);
+    el.removeEventListener('mouseup',    this.__mouseUp);
+    el.removeEventListener('mousewheel', this.__mouseWheel);
   },
-  mouseWheel: function(e){
+
+  _mouseWheel: function(e){
     var inverted = e.webkitDirectionInvertedFromDevice,
         delta = e.wheelDeltaY * (inverted ? 0.5 : -0.5),
         candidatePosition = this.scroller.__scrollTop + delta;
+
     if ((candidatePosition >= 0) && (candidatePosition <= this.scroller.__maxScrollTop)) {
       //TODO: this.showScrollbar()
       this.scroller.scrollBy(0, delta, true);
     }
+
     return false;
   },
 
   beginScroll: function(touches, timeStamp) {
     this.scroller.doTouchStart(touches, timeStamp);
   },
+
   continueScroll: function(touches, timeStamp) {
     this.scroller.doTouchMove(touches, timeStamp);
   },
+
   endScroll: function(timeStamp) {
     this.scroller.doTouchEnd(timeStamp);
   },
 
-  touchStart: function(e){
+  _touchStart: function(e){
     if (e.touches) {
       this.beginScroll(e.touches, e.timeStamp);
     }
     return false;
   },
-  touchMove: function(e){
+
+  _touchMove: function(e){
     if (e.touches) {
       this.continueScroll(e.touches, e.timeStamp);
     }
     return false;
   },
-  touchEnd: function(e){
+
+  _touchEnd: function(e){
     this.endScroll(e.timeStamp);
     return false;
   },
-  mouseDown: function(e){
+
+  _mouseDown: function(e){
     this.beginScroll([e], e.timeStamp);
     return false;
   },
-  mouseMove: function(e){
+
+  _mouseMove: function(e){
     this.continueScroll([e], e.timeStamp);
     return false;
   },
-  mouseUp: function(e){
+
+  _mouseUp: function(e){
     this.endScroll(e.timeStamp);
     return false;
   },
+
   scrollTo: function(y, animate) {
     if (animate === undefined) {
       animate = true;
