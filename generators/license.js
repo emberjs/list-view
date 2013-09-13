7d2e56a37a8068f9538a0d6bdd1c8245539f6dff
Kris Selden <kris.selden@gmail.com>
2013-09-13 14:11:06 -0700

    fix license

--- generators/license.js
+++ generators/license.js
@@ -1,7 +1,5 @@
 // ==========================================================================
-// Project:   Ember Data
-// Copyright: ©2011-2012 Tilde Inc. and contributors.
-//            Portions ©2011 Living Social Inc. and contributors.
-// License:   Licensed under MIT license (see license.js)
+// Project:   Ember ListView
+// Copyright: ©2012-2013 Erik Bryn, Yapp Inc., and contributors.
+// License:   Licensed under MIT license
 // ==========================================================================
-
