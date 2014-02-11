3ad983a822ee5c2604fea127279ccfae95d527c7
Stefan Penner <stefan.penner@gmail.com>
2014-02-11 17:06:44 -0500

    reduce extra gets/sets

--- packages/list-view/lib/reusable_list_item_view.js
+++ packages/list-view/lib/reusable_list_item_view.js
@@ -5,19 +5,21 @@ var get = Ember.get, set = Ember.set;
 Ember.ReusableListItemView = Ember.View.extend(Ember.ListItemViewMixin, {
   init: function(){
     this._super();
-    this.set('context', Ember.ObjectProxy.create());
+    var context = Ember.ObjectProxy.create();
+    this.set('context', context);
+    this._proxyContext = context;
   },
   isVisible: Ember.computed('context.content', function(){
     return !!this.get('context.content');
   }),
   updateContext: function(newContext){
-    var context = get(this, 'context.content');
+    var context = get(this._proxyContext, 'content');
     if (context !== newContext) {
       if (this.state === 'inDOM') {
         this.prepareForReuse(newContext);
       }
 
-      set(this, 'context.content', newContext);
+      set(this._proxyContext, 'content', newContext);
 
       if (newContext && newContext.isController) {
         set(this, 'controller', newContext);
