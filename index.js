8f3794f585f96a902cf1a921cd5eb11a0f4e78c2
Taras Mankovski <tarasm@gmail.com>
2015-03-05 19:07:00 -0500

    Added new directories & files for ember-cli-addon

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-var path = require('path');
+/* jshint node: true */
+'use strict';
 
 module.exports = {
-  name: 'list-view',
-  included: function (app) {
-    this._super.included(app);
-
-    app.import('vendor/list-view.amd.js', {
-      exports: {
-        'list-view/main': ['default'],
-        'list-view/helper': ['default'],
-        'list-view/list_item_view': ['default'],
-        'list-view/list_item_view_mixin': ['default'],
-        'list-view/list_view': ['default'],
-        'list-view/list_view_helper': ['default'],
-        'list-view/list_view_mixin': ['default']
-      }
-    });
-
-  },
-  treeForVendor: function () {
-    var dist = this._unwatchedTreeGenerator(path.join(__dirname, 'dist'));
-    return this.pickFiles(dist, {
-      srcDir: '/',
-      files: ['list-view.amd.js'],
-      destDir: '/'
-    });
-  }
+  name: 'list-view'
 };
