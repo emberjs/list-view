b1f2d30673064b9c19925db7b7b38fd25c4b7337
Taras Mankovski <tarasm@gmail.com>
2015-04-02 13:48:20 -0400

    Removes attrNode filter - no longer necessary with Ember 1.11.1

--- addon/list-view-mixin.js
+++ addon/list-view-mixin.js
@@ -825,10 +825,7 @@ export default Ember.Mixin.create({
     @method getReusableChildViews
   */
   getReusableChildViews: function() {
-    return Ember.EnumerableUtils.filter(this._childViews, function(view) {
-      // Ember 1.11 has virtual views, such as AttrNodes that we don't want to reuse
-      return !('attrName' in view);
-    });
+    return this._childViews;
   },
 
   /**
