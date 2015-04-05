3942c9f14b6e8a7c28ac3f2ff132c28dcadd52c7
Taras Mankovski <tarasm@gmail.com>
2015-04-05 11:20:33 -0400

    Upgraded Ember Try to 0.0.4 & added Ember 1.11.1 & Ember Canary

---
+++ config/ember-try.js
@@ -0,0 +1,22 @@
+module.exports = {
+  scenarios: [
+    {
+      name: 'Ember 1.10',
+      dependencies: {
+        ember: '1.10.0'
+      }
+    },
+    {
+      name: 'ember-1.11.1',
+      dependencies: {
+        ember: '1.11.1'
+      }
+    },
+    {
+      name: 'Ember canary',
+      dependencies: {
+        ember: 'canary'
+      }
+    }
+  ]
+};
